9928e5bc3ad87c3de2688e36bcad96cfdb2d5788
Nicolai Krogh Jørgensen <miberen@hotmail.com>
2015-04-23 21:52:58 +0200

    More features

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,11 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace ConsoleApplication1
 {
+
     class Program
     {
-        public static List<String> FilterSignal(int size, List<Double> original)
+        
+        public static List<string> FilterSignal(int size, List<double> original)
         {
             double[] orgArray = original.ToArray();
             List<String> newList = new List<string>(original.Count);
@@ -40,8 +44,38 @@ namespace ConsoleApplication1
 
             return newList;
         }
+
+        public static List<string> JoinTimNum(List<string> list1, List<string> list2)
+        {
+            List<string> done = new List<string>();
+
+            for (int i = 0; i < list2.Count-1; i++)
+            {
+                done.Add(Convert.ToDouble(list1[i])/1000 + "\t" + list2[i]);
+            }
+
+            return done;
+        }
+
+        public static List<string> JoinTimNumIBI(List<string> list1, List<string> list2)
+        {
+            List<string> done = new List<string>();
+            int[] intermediateNums = new int[list1.Count];
+
+
+            for (int i = 0; i < list1.Count - 1; i++)
+            {
+                done.Add(Convert.ToDouble(list1[i]) / 1000 + "\t" + list2[i]);
+            }
+
+            return done;
+        }
         static void Main(string[] args)
         {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
             int blockSize = 50;
             int _thresh = 850;
             List<string> _IBI = new List<string>();
@@ -49,12 +83,13 @@ namespace ConsoleApplication1
             int previousBeat = 0;
             List<int> IBItimings = new List<int>();
             List<int> IBImeasurements = new List<int>();
+            List<int> IBIIndex = new List<int>();
             List<double> EDAnumbers = new List<double>();
             List<double> EDATimings = new List<double>();
             List<string> EDAnew = new List<string>();
 
 
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataKiril.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\AnonUser\Documents\SignalBllshit\data\dataKiril.txt");
 
             foreach (string line in lines)
             {
@@ -80,6 +115,7 @@ namespace ConsoleApplication1
             {
                 if (IBImeasurements[i] >= _thresh && i > (currentBeat + 20) )
                 {
+                    IBIIndex.Add(i);
                     previousBeat = currentBeat;
                     currentBeat = i;
                     int timeSince = IBItimings[currentBeat] - IBItimings[previousBeat];
@@ -88,9 +124,11 @@ namespace ConsoleApplication1
             }
             EDAnew = FilterSignal(blockSize, EDAnumbers);
 
-            //File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataIBI.txt", _IBI);
-            //File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataEDA.txt", EDAnumbers);
-            File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataEDAfiltered.txt", EDAnew);
+            File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataIBI.txt", _IBI);
+            File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataIBITimNum.txt", _IBI);
+            File.WriteAllLines(@"C:\Users\AnonUser\Documents\SignalBllshit\data\dataEDAtimings.txt", EDATimings.Select(Convert.ToString));
+            File.WriteAllLines(@"C:\Users\AnonUser\Documents\SignalBllshit\data\dataEDAfiltered.txt", EDAnew);
+            File.WriteAllLines(@"C:\Users\AnonUser\Documents\SignalBllshit\data\dataEDATimNum.txt", JoinTimNum(new List<string>(EDATimings.Select(Convert.ToString)), EDAnew));
 
 
             // Keep the console window open in debug mode.
