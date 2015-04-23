60665f8d97e047c57245a10eed2ba60e88f352f0
Nicolai Krogh <miberen@hotmail.com>
2015-04-23 14:24:58 +0200

    Intiital

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static List<String> FilterSignal(int size, List<Double> original)
+        {
+            double[] orgArray = original.ToArray();
+            List<String> newList = new List<string>(original.Count);
+            string[] newArray = new string[original.Count];
+
+            for (int i = size/2; i < (original.Count - 1) - (size/2); i++)
+            {
+                double value = 0;            
+                for (int y = i - size/2; y < i + (size/2); y++)
+                {
+                    int o = i - size/2;
+                    value += orgArray[y];
+                }
+                newArray[i] = Convert.ToString(value/size);
+            }
+
+            for (int i = 0; i < size/2; i++)
+            {
+                newArray[i] = newArray[size/2];
+            }
+
+            for (int i = original.Count-(size/2)-1; i < original.Count; i++)
+            {
+
+                newArray[i] = newArray[original.Count-(size/2)-2];
+            }
+            newList = newArray.ToList();
+
+            return newList;
+        }
+        static void Main(string[] args)
+        {
+            int blockSize = 50;
+            int _thresh = 850;
+            List<string> _IBI = new List<string>();
+            int currentBeat = 0;
+            int previousBeat = 0;
+            List<int> IBItimings = new List<int>();
+            List<int> IBImeasurements = new List<int>();
+            List<double> EDAnumbers = new List<double>();
+            List<double> EDATimings = new List<double>();
+            List<string> EDAnew = new List<string>();
+
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataKiril.txt");
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("H"))
+                {
+                    string line2 = line.Trim(new char[]{'H', ','});
+                    string[] words = line2.Split('\t');
+                    IBItimings.Add(Convert.ToInt32(words[0]));
+                    IBImeasurements.Add(Convert.ToInt32(words[1]));
+                    Array.Clear(words,0, words.Length);
+                }
+                if (line.Contains("E"))
+                {
+                    string line2 = line.Trim(new char[] { 'E', ',' });
+                    string[] words = line2.Split('\t');
+                    EDATimings.Add(Convert.ToDouble(words[0]));
+                    EDAnumbers.Add(Convert.ToDouble(words[1]));
+                    Array.Clear(words, 0, words.Length);
+                }
+            }
+
+            for (int i = 0; i < IBImeasurements.Count - 1; i++)
+            {
+                if (IBImeasurements[i] >= _thresh && i > (currentBeat + 20) )
+                {
+                    previousBeat = currentBeat;
+                    currentBeat = i;
+                    int timeSince = IBItimings[currentBeat] - IBItimings[previousBeat];
+                    _IBI.Add(Convert.ToString(timeSince));
+                }
+            }
+            EDAnew = FilterSignal(blockSize, EDAnumbers);
+
+            //File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataIBI.txt", _IBI);
+            //File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataEDA.txt", EDAnumbers);
+            File.WriteAllLines(@"C:\Users\AnonUser\Documents\Processing\Reader\data\dataEDAfiltered.txt", EDAnew);
+
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }      
+    }
+}
